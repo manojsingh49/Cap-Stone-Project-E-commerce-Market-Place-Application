959802a58ca616ed19d7c4681c01b30b34ec906f
Manoj Singh <mmsingh1205@gmail.com>
2024-06-11 23:14:56 -0500

    added folder and files

---
+++ spring-CapStoneProj/eCommerceMarketPlaceProj/src/main/java/com/eCommerce/eCommerceMarketPlaceProj/ECommerceMarketPlaceProjApplication.java
@@ -0,0 +1,13 @@
+package com.eCommerce.eCommerceMarketPlaceProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMarketPlaceProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceMarketPlaceProjApplication.class, args);
+	}
+
+}
