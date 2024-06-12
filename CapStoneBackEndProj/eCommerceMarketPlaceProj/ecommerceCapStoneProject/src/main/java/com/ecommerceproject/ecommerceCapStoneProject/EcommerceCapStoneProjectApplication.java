c2d63de1b3bc8ab13ba6b03f101ac699a5502a39
Manoj Singh <mmsingh1205@gmail.com>
2024-06-12 14:06:34 -0500

    Added package and classes.

---
+++ CapStoneBackEndProj/eCommerceMarketPlaceProj/ecommerceCapStoneProject/src/main/java/com/ecommerceproject/ecommerceCapStoneProject/EcommerceCapStoneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerceproject.ecommerceCapStoneProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCapStoneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCapStoneProjectApplication.class, args);
+	}
+
+}
---
+++ CapStoneBackEndProj/eCommerceMarketPlaceProj/ecommerceCapStoneProject/src/test/java/com/ecommerceproject/ecommerceCapStoneProject/EcommerceCapStoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerceproject.ecommerceCapStoneProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceCapStoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
